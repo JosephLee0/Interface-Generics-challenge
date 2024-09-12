35ddab39670a6e025a858b75c452677daf373516
Joseph Olaniyan <josepholaniyan88@gmail.com>
2024-09-11 23:08:58 -0400

    final commit

---
+++ src/River.java
@@ -0,0 +1,16 @@
+public class River extends Line {
+
+        private String name;
+
+        public River(String name, String... location) {
+            super(location);
+            this.name = name;
+        }
+
+        @Override
+        public String toString() {
+            return name + "River";
+        }
+
+
+}
